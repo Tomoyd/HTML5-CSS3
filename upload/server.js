e5e96c1002a10f016ba4698618dd1b7798c299f6
Tomoyd <ydh9419@163.com>
2021-04-21 20:11:42 +0800

    [NFF]分片上传

---
+++ upload/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const server = http.createServer();
+
+server.on("request", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.status = 200;
+    res.end();
+    return;
+  }
+});
